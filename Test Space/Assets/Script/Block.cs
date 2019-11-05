34bfcb28b817c52f10c896850e47178a971044ca
dhon2407-pc <55686321+dhon2407@users.noreply.github.com>
2019-11-05 13:38:21 +0800

    Grid adjacent counting setup

---
+++ Test Space/Assets/Script/Block.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour
+{
+    [HideInInspector] public TapPosition onTap;
+    private Color color;
+    private Image image;
+    private int col;
+    private int row;
+
+    private void Awake()
+    {
+        onTap = new TapPosition();
+        image = GetComponent<Image>();
+        color = Random.Range(0, 100) > 50 ? Color.Blue : Color.Red;
+    }
+
+    private void Start()
+    {
+        image.color = color == Color.Blue ? UnityEngine.Color.blue : UnityEngine.Color.red;
+    }
+
+    public void SetPosition(int col, int row)
+    {
+        this.col = col;
+        this.row = row;
+    }
+
+    public void Clicked()
+    {
+        onTap.Invoke(col, row);
+    }
+
+    public enum Color
+    {
+        Red,
+        Blue
+    }
+
+    public class TapPosition : UnityEvent<int, int> { };
+}
---
+++ Test Space/Assets/Script/Board.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] private GameObject block = null;
+
+    private Block[,] blocks;
+    private GridLayoutGroup layout;
+
+    private void Awake()
+    {
+        layout = GetComponent<GridLayoutGroup>();
+        blocks = new Block[layout.constraintCount, layout.constraintCount];
+
+        for (int row = 0; row < layout.constraintCount; row++)
+        {
+            for (int col = 0; col < layout.constraintCount; col++)
+            {
+                blocks[col, row] = Instantiate(block, transform).GetComponent<Block>();
+                blocks[col, row].SetPosition(col, row);
+                blocks[col, row].onTap.AddListener(GetChainingAdjacentSameColor);
+            }
+        }
+    }
+
+    public void GetChainingAdjacentSameColor(int startCol, int startRow)
+    {
+        Debug.Log("Tap at " + startCol + " : " + startRow);
+    }
+}
