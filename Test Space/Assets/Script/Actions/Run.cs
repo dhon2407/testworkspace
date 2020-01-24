4935cdb2bb43c10dce526a2a4329fee08a458a4d
dhon2407-pc <55686321+dhon2407@users.noreply.github.com>
2020-01-25 06:18:27 +0800

    New unity version

--- Test Space/Assets/Script/Actions/Run.cs
+++ Test Space/Assets/Script/Actions/Run.cs
@@ -6,27 +6,28 @@ namespace Actions
     [CreateAssetMenu(fileName = "Run Action", menuName = "Actions/Run")]
     public class Run : CharacterAction
     {
-        [SerializeField] private int speedPercentIncrease = 200;
+        [SerializeField] private int speedPercentIncrease = 150;
 
         private bool _running;
+        private int _speedBoost;
         
         public override void Execute(ICharacterController characterController)
         {
+            InitializeCharacter(characterController);
+            
             if (!Character.CanRun || _running) return;
             
-            InitializeCharacter(characterController);
             var currentMovespeed = Character.Movespeed;
-
-            Character.Movespeed = (int) (currentMovespeed + (currentMovespeed * (speedPercentIncrease / 100f)));
+            _speedBoost = (int) (currentMovespeed * (speedPercentIncrease / 100f));
+            Character.Movespeed += _speedBoost;
             _running = true;
         }
 
         public override void Cancel(ICharacterController characterController)
         {
             if (!_running) return;
-            
-            var currentMovespeed = Character.Movespeed;
-            Character.Movespeed = (int) (currentMovespeed - (currentMovespeed * (speedPercentIncrease / 100f)));
+
+            Character.Movespeed -= _speedBoost;
             _running = false;
         }
     }
