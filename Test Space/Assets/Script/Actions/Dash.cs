58afeb90c896f1c85fad48f34125056b86e364c5
dhon2407-pc <55686321+dhon2407@users.noreply.github.com>
2020-01-25 18:16:12 +0800

    Removed Dash (remake soon)

--- Test Space/Assets/Script/Actions/Dash.cs
+++ Test Space/Assets/Script/Actions/Dash.cs
@@ -9,70 +9,19 @@ namespace Actions
     [CreateAssetMenu(fileName = "Dash Simple", menuName = "Actions/Simple Dash")]
     public class Dash : CharacterAction
     {
-        [SerializeField] private float speedMultiplier = 2f;
-        [SerializeField] private float distance = 0.5f;
-
-        [SerializeField]
-        private bool _onDash;
-        private Vector2 _dashVelocity;
-        private Vector2 _initialPosition;
-
         public override void Execute(ICharacterController characterController)
         {
-            if (_onDash) return;
-            
-            InitializeCharacter(characterController);
-
-            Timing.RunCoroutine(ExecuteDash());
+            throw new NotImplementedException();
         }
-        
+
         public override void Cancel(ICharacterController characterController)
-        { }
+        {
+            throw new NotImplementedException();
+        }
 
         public override void Hold(ICharacterController characterController)
-        { }
-
-        private IEnumerator<float> ExecuteDash()
         {
-            InitializeDash();
-            
-            Controller.DisableInputs = true;
-            Controller.DisableGravity = true;
-
-            var remainingDistance = distance;
-            while (remainingDistance > 0)
-            {
-                if (Mathf.Abs(_dashVelocity.x) * speedMultiplier > remainingDistance)
-                    _dashVelocity.x = (remainingDistance * Mathf.Sign(_dashVelocity.x) / speedMultiplier);
-
-                remainingDistance -= Mathf.Abs(_dashVelocity.x * speedMultiplier);
-
-                UpdateVelocity(_dashVelocity);
-                yield return Timing.WaitForOneFrame;
-            }
-            
-            Controller.DisableInputs = false;
-            Controller.DisableGravity = false;
-
-             _onDash = false;
-        }
-
-        private void InitializeDash()
-        {
-            _initialPosition = Character.Position;
-            var direction = Mathf.Sign(Controller.CurrentVelocity.x);
-            _dashVelocity = Vector2.right * direction;
-            _onDash = true;
-        }
-
-        private void UpdateVelocity(Vector2 dashVelocity)
-        {
-            Controller.UpdateVelocity(dashVelocity * speedMultiplier);
-        }
-
-        private void Awake()
-        {
-            _onDash = false;
+            throw new NotImplementedException();
         }
     }
 }
